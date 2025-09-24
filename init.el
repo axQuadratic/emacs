5048e327a16fba009eeedf310d0423fc03480efb
Nils <spuggel0@gmail.com>
2025-09-24 22:32:46 +0200

    Ensured consistent indentation by default through violence

--- init.el
+++ init.el
@@ -1,29 +1,43 @@
 (require-theme 'modus-themes)
 (require 'package)
+(require 'rust-mode)
 
 ;; Enable the MELPA repo for package.el
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; Extrude variables added by custom to their own file
-(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(setq-default custom-file (locate-user-emacs-file "custom-vars.el"))
 (load custom-file 'noerror 'nomessage)
 
 ;; Use-packages are loaded from this file
-(setq package-file (locate-user-emacs-file "installed-packages.el"))
+(setq-default package-file (locate-user-emacs-file "installed-packages.el"))
 (load package-file 'noerror 'nomessage)
 
+;; Hooks and language-specific settings are loaded from this file
+(setq-default lang-file (locate-user-emacs-file "language-settings.el"))
+(load lang-file 'noerror 'nomessage)
+
 ;; Functionality
-(setq org-return-follows-link t
+(setq-default org-return-follows-link t
       case-fold-search t
       truncate-lines t
       inhibit-startup-message t
-      indent-tabs-mode nil
       tab-width 4)
 
-(menu-bar-mode t) ; I am told this is helpful for discoverability
+;; Major modes love to clobber this one, and I *absolutely do not want it set to anything other than this*
+(put 'indent-line-function 'permanent-local t)
+(setq-default indent-line-function 'tab-to-tab-stop)
+
+;; If that still doesn't stop them, here's the nuclear option
+(add-hook 'after-change-major-mode-hook
+		  (lambda ()
+			(setq indent-line-function 'tab-to-tab-stop)))
+
+(menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(indent-tabs-mode -1)
 (recentf-mode 1)
 (save-place-mode 1)
 (blink-cursor-mode 0) 
@@ -35,13 +49,13 @@
 ;; Corfu settings
 (global-corfu-mode 1)
 
-(setq corfu-auto t
-      corfu-quit-no-match t)
+(setq-default corfu-auto t
+	  corfu-quit-no-match t)
 
 ;; Visuals
 (set-frame-font "CozetteVector 13" nil t)
 
-(setq modus-vivendi-palette-overrides
+(setq-default modus-vivendi-palette-overrides
       '((cursor "#ee7f01")
 		(bg-main "#070707")
 		(bg-dim "#0f0f0f")
@@ -72,3 +86,4 @@
 		(fg-completion-match-3 "be4f00")))
 
 (load-theme 'modus-vivendi t)
+(put 'upcase-region 'disabled nil)
